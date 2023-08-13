836ef7387d89107873e4c26a95d3f8fd5ff37d48
earfboyo <93812479+earfboyo@users.noreply.github.com>
2023-08-13 17:33:36 -0400

    Create index.m

---
+++ index.m
@@ -0,0 +1,22 @@
+       ---
+---
+
+# Welcome to my Art Studio
+
+joela R&D: A Component of Exploration for Insubordinate Systems
+
+## Recent Artworks
+
+- **Piece 1**: A brief description of this artwork. 
+- **Piece 2**: Another brief description.
+- ... and so on.
+
+## Blog Posts
+
+- [Title of your latest blog post](#)
+- [Another interesting post title](#)
+- ... and links to more posts.
+
+### Contact
+
+Feel free to [email me](mailto:youremail@example.com) for collaborations or inquiries.
